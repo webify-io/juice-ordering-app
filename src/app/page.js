965825f99fc985fe3ffd8dbc399e6e9fb72577cc
webify-io <webify.projects@gmail.com>
2024-04-29 16:02:02 +0200

    Update Nex.js to latest version and updated footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
-import Header from '@/components/layout/Header';
-import Hero from '@/components/layout/Hero';
-import HomeMenu from '@/components/layout/HomeMenu';
+import Hero from '../components/layout/Hero';
+import HomeMenu from '../components/layout/HomeMenu';
 import SectionHeaders from '../components/layout/SectionHeaders';
 
 export default function Home() {
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,29 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+
 export default function Footer() {
+	const [currentYear, setCurrentYear] = useState(null);
+
+	useEffect(() => {
+		const year = new Date().getFullYear();
+		setCurrentYear(year);
+	}, []);
+
 	return (
 		<footer className="border-t p-8 text-center text-gray-400 mt-16">
-			&copy; 2024 All Rights Reserved | Powered by Webify
+			<div>
+				&copy; {currentYear} | All Rights Reserved | Powered by
+				<a
+					href="https://webify-io.github.io/webify/"
+					target="_blank"
+					rel="noopener noreferrer"
+					style={{ marginLeft: '6px' }}
+					className="text-blue-400 font-semibold"
+				>
+					Webify<sup>&reg;</sup>
+				</a>
+			</div>
 		</footer>
 	);
 }
