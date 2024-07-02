ae761568252dfd1b8850050d47be33041c6b0f7e
webify-io <webify.projects@gmail.com>
2024-07-02 12:43:04 +0200

    Deleted node_modules & tsconfig.js for Prod build
    Updated: import { MongoDBAdapter } from '@next-auth/mongodb-adapter';

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,6 @@ import User from '../../../../models/User';
 import NextAuth, { getServerSession } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import clientPromise from '../../../../libs/mongoConnect';
 import { UserInfo } from '../../../../models/UserInfo';
 
